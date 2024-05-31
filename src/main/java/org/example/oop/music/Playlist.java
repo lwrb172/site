8172fb510bdcd23b4ee3565beb74b42aea5864ac
lwrb172 <lwrbielak@gmail.com>
2024-05-31 18:18:35 +0200

    lab8z1a

---
+++ src/main/java/org/example/oop/music/Playlist.java
@@ -0,0 +1,6 @@
+package org.example.oop.music;
+
+import java.util.ArrayList;
+
+public class Playlist extends ArrayList<Song> {
+}
---
+++ src/main/java/org/example/oop/music/Song.java
@@ -0,0 +1,4 @@
+package org.example.oop.music;
+
+public record Song(String artist, String title, int duration) {
+}
