5feaf4530fbfc5c1f0e5c4756998836f648255bd
lwrb172 <lwrbielak@gmail.com>
2024-05-31 18:38:38 +0200

    lab8z1f

--- src/main/java/org/example/oop/music/Playlist.java
+++ src/main/java/org/example/oop/music/Playlist.java
@@ -3,4 +3,12 @@ package org.example.oop.music;
 import java.util.ArrayList;
 
 public class Playlist extends ArrayList<Song> {
+    public Song atSecond(int seconds) {
+        int sum = 0;
+        for (Song song : this) {
+            sum += song.getDuration();
+            if (sum >= seconds) return song;
+        }
+        return null;
+    }
 }
--- src/main/java/org/example/oop/music/Song.java
+++ src/main/java/org/example/oop/music/Song.java
@@ -1,4 +1,42 @@
 package org.example.oop.music;
 
-public record Song(String artist, String title, int duration) {
+//public record Song(String artist, String title, int duration) {}
+
+import java.util.Objects;
+
+public class Song {
+    public String artist;
+    public String title;
+    public int duration;
+
+    public Song(String artist, String title, int duration) {
+        this.artist = artist;
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return duration == song.duration && Objects.equals(artist, song.artist) && Objects.equals(title, song.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artist, title, duration);
+    }
 }
