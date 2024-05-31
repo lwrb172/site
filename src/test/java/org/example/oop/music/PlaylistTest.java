f70e821981c39b536d6bac8c0fc1a2fce5ffb03a
lwrb172 <lwrbielak@gmail.com>
2024-05-31 18:41:08 +0200

    lab8z1f - adjustments

--- src/test/java/org/example/oop/music/PlaylistTest.java
+++ src/test/java/org/example/oop/music/PlaylistTest.java
@@ -2,8 +2,7 @@ package org.example.oop.music;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlaylistTest {
     @Test
@@ -33,5 +32,21 @@ public class PlaylistTest {
         Playlist playlist = new Playlist();
         playlist.add(new Song("Heavy Young Heathens", "Being Evil Has a Price", 194));
         assertEquals(song,playlist.getFirst());
+    }
+
+    @Test
+    void atSecondTest() {
+        Song song1 = new Song("Heavy Young Heathens", "Being Evil Has a Price", 203);
+        Song song2 = new Song("The Forest Rangers", "John The Revelator", 334);
+        Song song3 = new Song("Lee DeWyze", "Blackbird Song", 254);
+        Playlist playlist = new Playlist();
+        playlist.add(song1);
+        playlist.add(song2);
+        playlist.add(song3);
+
+        assertEquals(song2, playlist.atSecond(350));
+        assertNull(playlist.atSecond(800));
+        assertEquals(song1, playlist.atSecond(200));
+        assertEquals(song3, playlist.atSecond(600));
     }
 }
